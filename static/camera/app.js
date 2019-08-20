41d5e445e36007e76f500b177388c48c02349511
th20023980 <tharun.p83@wipro.com>
2019-08-20 17:47:36 +0530

    First Commit

---
+++ static/camera/app.js
@@ -0,0 +1,36 @@
+// Set constraints for the video stream
+var constraints = { video: { facingMode: "user" }, audio: false };
+// Define constants
+const cameraView = document.querySelector("#camera--view"),
+    cameraOutput = document.querySelector("#camera--output"),
+    cameraSensor = document.querySelector("#camera--sensor"),
+    cameraTrigger = document.querySelector("#camera--trigger")
+// Access the device camera and stream to cameraView
+function cameraStart() {
+    navigator.mediaDevices
+        .getUserMedia(constraints)
+        .then(function(stream) {
+        track = stream.getTracks()[0];
+        cameraView.srcObject = stream;
+    })
+    .catch(function(error) {
+        console.error("Oops. Something is broken.", error);
+    });
+}
+// Take a picture when cameraTrigger is tapped
+cameraTrigger.onclick = function() {
+    cameraSensor.width = cameraView.videoWidth;
+    cameraSensor.height = cameraView.videoHeight;
+    cameraSensor.getContext("2d").drawImage(cameraView, 0, 0);
+    cameraOutput.src = cameraSensor.toDataURL("image/png");
+    //console.log(5+6);
+  //  var img = cameraSensor.toDataURL("image/png");
+    //console.log(img);
+    document.getElementById('custId').value = cameraOutput.src;
+  	document.getElementById("myForm").submit();
+    cameraOutput.classList.add("taken");
+
+
+};
+// Start the video stream when the window loads
+window.addEventListener("load", cameraStart, false);
